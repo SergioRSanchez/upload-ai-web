43276b42e4308d69bda04af417b2968dd4f1bb23
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-09-13 22:23:15 -0300

    feat: configure shadcn ui, radix ui, tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from './components/ui/button';
+
 export function App() {
 
   return (
-    <div>Hello World</div>
+    <div>
+      <h1>Hello World</h1>
+      <Button>Teste</Button>
+    </div>
   )
 }
